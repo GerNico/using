a3f7ffcbc3a172a18f9d29ad215f9a45ffaf8328
MykolaM-AVISPL <59696595+MykolaM-AVISPL@users.noreply.github.com>
2022-04-09 16:55:13 +0300

    Initial commit

---
+++ src/test/java/com/example/using/UsingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.using;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
